ff767f0b1a3d11212cbf98160ddf539e3fd4555c
JungLee-Dev <junghoonlee.dev@gmail.com>
2025-01-04 00:59:18 -0700

    add scripts, assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    [Header("Settings")]
+    public float JumpForce;
+
+    [Header("References")]
+    public Rigidbody2D PlayerRigidBody;
+    public Animator PlayerAnimator;
+    private bool isGrounded = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            PlayerAnimator.SetInteger("state", 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.name == "Platform") {
+            if(!isGrounded) {
+                PlayerAnimator.SetInteger("state", 2);
+            }
+            isGrounded = true;            
+        }
+    }
+}
