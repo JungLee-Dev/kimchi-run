1419ae30e96a242f2c4888a24e987f4c7bde3208
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2025-08-06 23:48:36 -0600

    Update Heart.cs

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -8,8 +8,7 @@ public class Heart : MonoBehaviour
     public int LiveNumber;    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {        
     }
 
     // Update is called once per frame
