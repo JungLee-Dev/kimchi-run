df89049a9e23b806bde70e11c3940960f17eb133
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2025-08-07 23:58:42 -0600

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,8 +6,7 @@ public class Mover : MonoBehaviour
     public float moveSpeed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {        
     }
 
     // Update is called once per frame
